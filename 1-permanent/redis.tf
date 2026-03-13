0dc1c0c4d94c91f5963df7f2e887074f089fd82e
ysheep0906 <wlq94814@naver.com>
2026-03-13 09:52:44 +0900

    fix: IAM 경로 수정

--- 1-permanent/redis.tf
+++
@@ -1,2 +0,0 @@
-# Redis moved to `2-ephemeral/redis.tf`.
-# Keep this file as a placeholder to avoid accidental reintroduction in permanent layer.
--- 2-ephemeral/iam.tf
+++ 2-ephemeral/iam.tf
@@ -1,24 +1,3 @@
-# RDS Lambda 역할
-resource "aws_iam_role" "stagelog_rds_lambda_role_managed" {
-  name = "stagelog-rds-lambda-role-managed"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-
-  managed_policy_arns = [
-    "arn:aws:iam::430118823715:policy/service-role/AWSLambdaBasicExecutionRole-dfc0340f-2e3b-4f7c-891c-98b53cfeccec",
-    "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-  ]
-}
-
 # EKS 역할
 resource "aws_iam_role" "stagelog_eks_role_managed" {
   name = "stagelog-eks-role-managed"
@@ -104,4 +83,41 @@ resource "aws_iam_role" "external_secrets" {
 resource "aws_iam_role_policy_attachment" "external_secrets_attach" {
   role       = aws_iam_role.external_secrets.name 
   policy_arn = aws_iam_policy.external_secrets.arn 
+}
+
+# IAM policy for ALB Ingress Controller
+resource "aws_iam_policy" "alb_ingress_controller_policy" {
+  name        = "ALBIngressControllerIAMPolicy-stagelog"
+  description = "IAM policy for ALB Ingress Controller to manage ALBs in EKS cluster."
+  policy      = file("${path.module}/alb-ingress-controller-policy.json")
+}
+
+# IAM role for ALB Ingress Controller
+resource "aws_iam_role" "alb_ingress_controller_role" {
+  name               = "ALBIngressControllerIAMRole-stagelog"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity" # EKS OIDC provider를 통한 웹 아이덴티티 연동
+        Effect = "Allow"
+        Principal = {
+            Federated = aws_iam_openid_connect_provider.eks_oidc.arn
+        }
+        Condition = { 
+            StringEquals = {
+                "${replace(aws_eks_cluster.stagelog-eks.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller" 
+            }
+        }
+      }
+    ]
+  })
+}
+
+# IAM policy attachment for ALB Ingress Controller
+resource "aws_iam_policy_attachment" "alb_ingress_controller_policy_attachment" {
+  name       = "ALBIngressControllerIAMPolicyAttachment-stagelog"
+  policy_arn = aws_iam_policy.alb_ingress_controller_policy.arn
+  roles      = [aws_iam_role.alb_ingress_controller_role.name]
 }
